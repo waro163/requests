65d7a611f1cb3f275fedde3aa1a04d6258d9b06e
Wang Rong <wangron@tesla.com>
2023-12-28 22:10:04 +0800

    [perf]update comment for hook

--- hook.go
+++ hook.go
@@ -14,7 +14,7 @@ type Hook interface {
 	OnRequestError(context.Context, *http.Request, error) error
 	// ProcessResponse handle this request and response after do requesting
 	ProcessResponse(context.Context, *http.Request, *http.Response) error
-	// OnRequestError is for when ProcessResponse return error, it will handle this error, if this function also return error, all the work will stop
+	// OnResponseError is for when ProcessResponse return error, it will handle this error, if this function also return error, all the work will stop
 	OnResponseError(context.Context, *http.Request, *http.Response, error) error
 }
 
